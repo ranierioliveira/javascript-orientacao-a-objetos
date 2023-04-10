b6e6aaefe30a96c12da0647b6eba968325da130e
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-04-10 14:02:46 -0300

    Métodos dentro de uma classe

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 class cliente{
     nome;
     cpf;
+}
+class contaCorrente{
     agencia;
     saldo;
+    sacar(valor){ //this.saldo representa o saldo da conta em questão
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
+
 const cliente1 = new cliente;
 const cliente2 = new cliente;
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteRicardo = new contaCorrente;
+
+contaCorrenteRicardo.agencia = 0001;
+contaCorrenteRicardo.saldo = 0;
+console.log(contaCorrenteRicardo.saldo); 
+
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(20);
+console.log(contaCorrenteRicardo.saldo);
+
+
+
+
+
 
 console.log(cliente1);
 console.log(cliente2);
