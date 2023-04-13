8961ebd0c2c347bfa4ee2db697b7cb2829a89e80
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-04-13 11:40:34 -0300

    return e early em métodos no JS

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ class cliente{
 class contaCorrente{
     agencia;
     //#saldo
-    saldo;
+    _saldo;
     sacar(valor){ //this.saldo representa o saldo da conta em questão
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
         }
     }
     depositar(valor){
-        if (valor > 0){
-            this.saldo += valor;
-        }
+        if (valor <= 0) return;
+        this._saldo += valor;
     }
 }
 
@@ -30,9 +30,11 @@ cliente2.cpf = 88822233309;
 const contaCorrenteRicardo = new contaCorrente;
 
 contaCorrenteRicardo.agencia = 0001;
-contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo._saldo = 0;
 
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
