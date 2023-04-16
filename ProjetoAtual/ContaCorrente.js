59c3120879fa545e4558375fd6799607e28afddd
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-04-16 13:57:11 -0300

    Composição de classes

--- ProjetoAtual/ContaCorrente.js
+++ ProjetoAtual/ContaCorrente.js
@@ -1,15 +1,26 @@
 export class ContaCorrente{
     agencia;
+    cliente;
+
     //#saldo
     _saldo;
+    
+
     sacar(valor){ //this.saldo representa o saldo da conta em questão
         if(this._saldo >= valor){
             this._saldo -= valor;
             return valor;
         }
     }
+
     depositar(valor){
         if (valor <= 0) return;
         this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        
+
+
     }
 }
--- ProjetoAtual/index.js
+++ ProjetoAtual/index.js
@@ -6,20 +6,20 @@ const cliente2 = new Cliente;
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-const contaCorrenteRicardo = new ContaCorrente;
 
+const contaCorrenteRicardo = new ContaCorrente;
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo._saldo = 0;
+contaCorrenteRicardo.cliente = cliente1;
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente;
+conta2.agencia = 1001;
+conta2.cliente = cliente2;
+conta2._saldo = 0;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
 
-console.log(valorSacado);
 
-console.log(contaCorrenteRicardo);
+console.log(conta2);
