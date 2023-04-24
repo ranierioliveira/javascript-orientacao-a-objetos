072614c8cab4c4223aec24d1fa2afd3aa619f816
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-04-23 21:00:42 -0300

    Null e undefined

--- ProjetoAtual/index.js
+++ ProjetoAtual/index.js
@@ -2,12 +2,12 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente;
-const cliente2 = new Cliente;
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+
+// const cliente2 = new Cliente;
+// cliente2.nome = "Alice";
+// cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente;
 contaCorrenteRicardo.agencia = 1001;
@@ -16,8 +16,10 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(100);
 
 const conta2 = new ContaCorrente;
+conta2.cliente = new Cliente;
+conta2.cliente.nome = "Alice"
+conta2.cliente.cpf = 88822233309;
 conta2.agencia = 1001;
-conta2.cliente = cliente2;
 conta2._saldo = 0;
 
 contaCorrenteRicardo.transferir(50, conta2);
